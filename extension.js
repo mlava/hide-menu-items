5cacd65ff8c4678dfb7411d69d242053c8c9dc8d
Mark Lavercombe <mlava@users.noreply.github.com>
2023-01-23 07:57:15 +1100

    Add files via upload

--- extension.js
+++ extension.js
@@ -3,6 +3,7 @@ var hmiCal = false;
 var hmi3DM = false;
 var hmiPW = false;
 var hmiHelp = false;
+var hmiRS = false;
 
 export default {
     onload: ({ extensionAPI }) => {
@@ -12,32 +13,38 @@ export default {
                 {
                     id: "hmi-PF",
                     name: "Page Filter",
-                    description: "Turn on to hide the Page Filter button on all platforms",
-                    action: { type: "select", items: ["Show", "Hide on Mobile", "Hide on All Platforms"], onChange: (evt) => { setHMI(evt, 1); } },
+                    description: "Turn on to hide the Page Filter button",
+                    action: { type: "select", items: ["Show on All Platforms", "Hide on Mobile", "Hide on All Platforms"], onChange: (evt) => { setHMI(evt, 1); } },
                 },
                 {
                     id: "hmi-Cal",
                     name: "Calendar",
-                    description: "Turn on to hide the Calendar button on all platforms",
-                    action: { type: "select", items: ["Show", "Hide on Mobile", "Hide on All Platforms"], onChange: (evt) => { setHMI(evt, 2); } },
+                    description: "Turn on to hide the Calendar button",
+                    action: { type: "select", items: ["Show on All Platforms", "Hide on Mobile", "Hide on All Platforms"], onChange: (evt) => { setHMI(evt, 2); } },
                 },
                 {
                     id: "hmi-3DM",
                     name: "Three-Dot Menu",
-                    description: "Turn on to hide the Three-Dot Menu button on all platforms",
-                    action: { type: "select", items: ["Show", "Hide on Mobile", "Hide on All Platforms"], onChange: (evt) => { setHMI(evt, 3); } },
+                    description: "Turn on to hide the Three-Dot Menu button",
+                    action: { type: "select", items: ["Show on All Platforms", "Hide on Mobile", "Hide on All Platforms"], onChange: (evt) => { setHMI(evt, 3); } },
                 },
                 {
                     id: "hmi-PW",
                     name: "Page Width",
-                    description: "Turn on to hide the Page Width button on all platforms",
-                    action: { type: "select", items: ["Show", "Hide on Mobile", "Hide on All Platforms"], onChange: (evt) => { setHMI(evt, 4); } },
+                    description: "Turn on to hide the Page Width button",
+                    action: { type: "select", items: ["Show on All Platforms", "Hide on Mobile", "Hide on All Platforms"], onChange: (evt) => { setHMI(evt, 4); } },
                 },
                 {
                     id: "hmi-Help",
                     name: "Help",
-                    description: "Turn on to hide the Help button on all platforms",
-                    action: { type: "select", items: ["Show", "Hide on Mobile", "Hide on All Platforms"], onChange: (evt) => { setHMI(evt, 5); } },
+                    description: "Turn on to hide the Help button",
+                    action: { type: "select", items: ["Show on All Platforms", "Hide on Mobile", "Hide on All Platforms"], onChange: (evt) => { setHMI(evt, 5); } },
+                },
+                {
+                    id: "hmi-RS",
+                    name: "Right Sidebar",
+                    description: "Turn on to hide the Right Sidebar button",
+                    action: { type: "select", items: ["Show on All Platforms", "Hide on Mobile", "Hide on All Platforms"], onChange: (evt) => { setHMI(evt, 6); } },
                 },
             ]
         };
@@ -48,6 +55,7 @@ export default {
         hmi3DM = extensionAPI.settings.get("hmi-3DM");
         hmiPW = extensionAPI.settings.get("hmi-PW");
         hmiHelp = extensionAPI.settings.get("hmi-Help");
+        hmiRS = extensionAPI.settings.get("hmi-RS");
 
         function setHMI(evt, i) {
             if (i == 1) {
@@ -60,6 +68,8 @@ export default {
                 hmiPW = evt;
             } else if (i == 5) {
                 hmiHelp = evt;
+            } else if (i == 6) {
+                hmiRS = evt;
             }
             hideDIVs();
         }
@@ -70,42 +80,50 @@ export default {
         hmi3DM = false;
         hmiPW = false;
         hmiHelp = false;
+        hmiRS = false;
         hideDIVs();
     }
 }
 
 async function hideDIVs() {
     let topbar = document.querySelectorAll("div.rm-topbar > span.bp3-popover-wrapper");
+    console.info(topbar);
     if (window.roamAlphaAPI.platform.isMobile || window.roamAlphaAPI.platform.isMobileApp || window.roamAlphaAPI.platform.isTouchDevice || window.roamAlphaAPI.platform.isIOS) {
         // hide items if selected as mobile only or all platforms
         if (topbar.length > 0) {
             for (var i = 0; i < topbar.length; i++) {
                 if (topbar[i].childNodes[0]?.childNodes[0]?.childNodes[0]?.childNodes[0]?.childNodes[0]?.classList[1] == "bp3-icon-filter") {
-                    if (hmiPF != "Show") {
+                    if (hmiPF != "Show on All Platforms") {
                         topbar[i].style.display = "none";
                     } else {
                         topbar[i].style.display = "";
                     }
                 } else if (topbar[i].childNodes[0]?.childNodes[0]?.childNodes[0]?.classList[1] == "bp3-icon-calendar") {
-                    if (hmiCal != "Show") {
+                    if (hmiCal != "Show on All Platforms") {
                         topbar[i].style.display = "none";
                     } else {
                         topbar[i].style.display = "";
                     }
                 } else if (topbar[i].childNodes[0]?.childNodes[0]?.childNodes[0]?.childNodes[0]?.classList[3] == "bp3-icon-more") {
-                    if (hmi3DM != "Show") {
+                    if (hmi3DM != "Show on All Platforms") {
                         topbar[i].style.display = "none";
                     } else {
                         topbar[i].style.display = "";
                     }
                 } else if (topbar[i].childNodes[0]?.childNodes[0]?.childNodes[0]?.childNodes[0]?.classList[3] == "bp3-icon-horizontal-distribution") {
-                    if (hmiPW != "Show") {
+                    if (hmiPW != "Show on All Platforms") {
                         topbar[i].style.display = "none";
                     } else {
                         topbar[i].style.display = "";
                     }
                 } else if (topbar[i].childNodes[0]?.childNodes[0]?.classList[2] == "bp3-icon-help") {
-                    if (hmiHelp != "Show") {
+                    if (hmiHelp != "Show on All Platforms") {
+                        topbar[i].style.display = "none";
+                    } else {
+                        topbar[i].style.display = "";
+                    }
+                } else if (topbar[i].childNodes[0]?.childNodes[0]?.childNodes[0]?.classList[1] == "bp3-icon-menu-closed") {
+                    if (hmiRS != "Show on All Platforms") {
                         topbar[i].style.display = "none";
                     } else {
                         topbar[i].style.display = "";
@@ -143,6 +161,12 @@ async function hideDIVs() {
                     }
                 } else if (topbar[i].childNodes[0]?.childNodes[0]?.classList[2] == "bp3-icon-help") {
                     if (hmiHelp == "Hide on All Platforms") {
+                        topbar[i].style.display = "none";
+                    } else {
+                        topbar[i].style.display = "";
+                    }
+                } else if (topbar[i].childNodes[0]?.childNodes[0]?.childNodes[0]?.classList[1] == "bp3-icon-menu-closed") {
+                    if (hmiRS == "Hide on All Platforms") {
                         topbar[i].style.display = "none";
                     } else {
                         topbar[i].style.display = "";
