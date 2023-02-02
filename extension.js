5f45e91120c2d1483457e89048c7e7511e280dbb
Mark Lavercombe <mlava@users.noreply.github.com>
2023-02-03 10:18:13 +1100

    Add files via upload

--- extension.js
+++ extension.js
@@ -8,7 +8,7 @@ var hmiRS = false;
 export default {
     onload: ({ extensionAPI }) => {
         const config = {
-            tabTitle: "Hide Menu Items",
+            tabTitle: "Hide Topbar Buttons",
             settings: [
                 {
                     id: "hmi-PF",
@@ -73,6 +73,7 @@ export default {
             }
             hideDIVs();
         }
+        hideDIVs();
     },
     onunload: () => {
         hmiPF = false;
@@ -86,92 +87,251 @@ export default {
 }
 
 async function hideDIVs() {
+    var pf, pfSib, calendar, calendarSib, threeDot, threeDotSib, width, widthSib, help, helpSib, rightSidebar;
+
     let topbar = document.querySelectorAll("div.rm-topbar > span.bp3-popover-wrapper");
+    if (topbar.length > 0) {
+        for (var i = 0; i < topbar.length; i++) {
+            if (topbar[i].innerHTML.indexOf("bp3-icon-filter") !== -1) {
+                pf = topbar[i];
+                pfSib = pf.nextSibling;
+            } else if (topbar[i].innerHTML.indexOf("bp3-icon-calendar") !== -1) {
+                calendar = topbar[i];
+                calendarSib = calendar.nextSibling;
+            } else if (topbar[i].innerHTML.indexOf("bp3-icon-more") !== -1) {
+                threeDot = topbar[i];
+                threeDotSib = threeDot.nextSibling;
+            } else if (topbar[i].innerHTML.indexOf("bp3-icon-horizontal-distribution") !== -1) {
+                width = topbar[i];
+                widthSib = width.nextSibling;
+            } else if (topbar[i].innerHTML.indexOf("bp3-icon-help") !== -1) {
+                help = topbar[i];
+                helpSib = help.nextSibling;
+            } else if (topbar[i].innerHTML.indexOf("bp3-icon-menu-closed") !== -1) {
+                rightSidebar = topbar[i];
+            }
+        }
+    }
+
     if (window.roamAlphaAPI.platform.isMobile || window.roamAlphaAPI.platform.isMobileApp || window.roamAlphaAPI.platform.isTouchDevice || window.roamAlphaAPI.platform.isIOS) {
         // hide items if selected as mobile only or all platforms
-        if (topbar.length > 0) {
-            for (var i = 0; i < topbar.length; i++) {
-                if (topbar[i].childNodes[0]?.childNodes[0]?.childNodes[0]?.childNodes[0]?.childNodes[0]?.classList[1] == "bp3-icon-filter") {
-                    if (hmiPF != "Show on All Platforms") {
-                        topbar[i].style.display = "none";
-                    } else {
-                        topbar[i].style.display = "";
-                    }
-                } else if (topbar[i].childNodes[0]?.childNodes[0]?.childNodes[0]?.classList[1] == "bp3-icon-calendar") {
-                    if (hmiCal != "Show on All Platforms") {
-                        topbar[i].style.display = "none";
-                    } else {
-                        topbar[i].style.display = "";
-                    }
-                } else if (topbar[i].childNodes[0]?.childNodes[0]?.childNodes[0]?.childNodes[0]?.classList[3] == "bp3-icon-more") {
-                    if (hmi3DM != "Show on All Platforms") {
-                        topbar[i].style.display = "none";
-                    } else {
-                        topbar[i].style.display = "";
-                    }
-                } else if (topbar[i].childNodes[0]?.childNodes[0]?.childNodes[0]?.childNodes[0]?.classList[3] == "bp3-icon-horizontal-distribution") {
-                    if (hmiPW != "Show on All Platforms") {
-                        topbar[i].style.display = "none";
-                    } else {
-                        topbar[i].style.display = "";
-                    }
-                } else if (topbar[i].childNodes[0]?.childNodes[0]?.classList[2] == "bp3-icon-help") {
-                    if (hmiHelp != "Show on All Platforms") {
-                        topbar[i].style.display = "none";
-                    } else {
-                        topbar[i].style.display = "";
-                    }
-                } else if (topbar[i].childNodes[0]?.childNodes[0]?.childNodes[0]?.classList[1] == "bp3-icon-menu-closed") {
-                    if (hmiRS != "Show on All Platforms") {
-                        topbar[i].style.display = "none";
-                    } else {
-                        topbar[i].style.display = "";
-                    }
-                }
+        if (hmiPF != "Show on All Platforms") {
+            if (pf != undefined) {
+                pf.style.display = "none";
+            }
+            if (pfSib != undefined) {
+                pfSib.style.display = "none";
+            }
+        } else {
+            if (pf != undefined) {
+                pf.style.display = "";
+            }
+            if (pfSib != undefined) {
+                pfSib.style.display = "";
+            }
+        }
+        if (hmiCal != "Show on All Platforms") {
+            if (calendar != undefined) {
+                calendar.style.display = "none";
+            }
+            if (calendarSib != undefined) {
+                calendarSib.style.display = "none";
+            }
+        } else {
+            if (calendar != undefined) {
+                calendar.style.display = "";
+            }
+            if (calendarSib != undefined) {
+                calendarSib.style.display = "";
+            }
+        }
+        if (hmi3DM != "Show on All Platforms") {
+            if (threeDot != undefined) {
+                threeDot.style.display = "none";
+            }
+            if (threeDotSib != undefined) {
+                threeDotSib.style.display = "none";
+            }
+        } else {
+            if (threeDot != undefined) {
+                threeDot.style.display = "";
+            }
+            if (threeDotSib != undefined) {
+                threeDotSib.style.display = "";
+            }
+        }
+        if (hmiPW != "Show on All Platforms") {
+            if (width != undefined) {
+                width.style.display = "none";
+            }
+            if (widthSib != undefined) {
+                widthSib.style.display = "none";
+            }
+        } else {
+            if (width != undefined) {
+                width.style.display = "";
+            }
+            if (widthSib != undefined) {
+                widthSib.style.display = "";
+            }
+        }
+        if (hmiHelp != "Show on All Platforms") {
+            if (help != undefined) {
+                help.style.display = "none";
+            }
+            if (helpSib != undefined) {
+                helpSib.style.display = "none";
+            }
+        } else {
+            if (help != undefined) {
+                help.style.display = "";
+            }
+            if (helpSib != undefined) {
+                helpSib.style.display = "";
+            }
+        }
+        if (hmiRS != "Show on All Platforms") {
+            if (rightSidebar != undefined) {
+                rightSidebar.style.display = "none";
+            }
+        } else {
+            if (rightSidebar != undefined) {
+                rightSidebar.style.display = "";
             }
         }
     } else {
         // hide items only if selected for all platforms
-        if (topbar.length > 0) {
-            for (var i = 0; i < topbar.length; i++) {
-                if (topbar[i].childNodes[0]?.childNodes[0]?.childNodes[0]?.childNodes[0]?.childNodes[0]?.classList[1] == "bp3-icon-filter") {
-                    if (hmiPF == "Hide on All Platforms") {
-                        topbar[i].style.display = "none";
-                    } else {
-                        topbar[i].style.display = "";
-                    }
-                } else if (topbar[i].childNodes[0]?.childNodes[0]?.childNodes[0]?.classList[1] == "bp3-icon-calendar") {
-                    if (hmiCal == "Hide on All Platforms") {
-                        topbar[i].style.display = "none";
-                    } else {
-                        topbar[i].style.display = "";
-                    }
-                } else if (topbar[i].childNodes[0]?.childNodes[0]?.childNodes[0]?.childNodes[0]?.classList[3] == "bp3-icon-more") {
-                    if (hmi3DM == "Hide on All Platforms") {
-                        topbar[i].style.display = "none";
-                    } else {
-                        topbar[i].style.display = "";
-                    }
-                } else if (topbar[i].childNodes[0]?.childNodes[0]?.childNodes[0]?.childNodes[0]?.classList[3] == "bp3-icon-horizontal-distribution") {
-                    if (hmiPW == "Hide on All Platforms") {
-                        topbar[i].style.display = "none";
-                    } else {
-                        topbar[i].style.display = "";
-                    }
-                } else if (topbar[i].childNodes[0]?.childNodes[0]?.classList[2] == "bp3-icon-help") {
-                    if (hmiHelp == "Hide on All Platforms") {
-                        topbar[i].style.display = "none";
-                    } else {
-                        topbar[i].style.display = "";
-                    }
-                } else if (topbar[i].childNodes[0]?.childNodes[0]?.childNodes[0]?.classList[1] == "bp3-icon-menu-closed") {
-                    if (hmiRS == "Hide on All Platforms") {
-                        topbar[i].style.display = "none";
-                    } else {
-                        topbar[i].style.display = "";
-                    }
-                }
+        if (hmiPF == "Hide on All Platforms") {
+            if (pf != undefined) {
+                pf.style.display = "none";
+            }
+            if (pfSib != undefined) {
+                pfSib.style.display = "none";
+            }
+        } else {
+            if (pf != undefined) {
+                pf.style.display = "";
+            }
+            if (pfSib != undefined) {
+                pfSib.style.display = "";
+            }
+        }
+        if (hmiCal == "Hide on All Platforms") {
+            if (calendar != undefined) {
+                calendar.style.display = "none";
+            }
+            if (calendarSib != undefined) {
+                calendarSib.style.display = "none";
+            }
+        } else {
+            if (calendar != undefined) {
+                calendar.style.display = "";
+            }
+            if (calendarSib != undefined) {
+                calendarSib.style.display = "";
+            }
+        }
+        if (hmi3DM == "Hide on All Platforms") {
+            if (threeDot != undefined) {
+                threeDot.style.display = "none";
+            }
+            if (threeDotSib != undefined) {
+                threeDotSib.style.display = "none";
+            }
+        } else {
+            if (threeDot != undefined) {
+                threeDot.style.display = "";
+            }
+            if (threeDotSib != undefined) {
+                threeDotSib.style.display = "";
+            }
+        }
+        if (hmiPW == "Hide on All Platforms") {
+            if (width != undefined) {
+                width.style.display = "none";
+            }
+            if (widthSib != undefined) {
+                widthSib.style.display = "none";
+            }
+        } else {
+            if (width != undefined) {
+                width.style.display = "";
+            }
+            if (widthSib != undefined) {
+                widthSib.style.display = "";
+            }
+        }
+        if (hmiHelp == "Hide on All Platforms") {
+            if (help != undefined) {
+                help.style.display = "none";
+            }
+            if (helpSib != undefined) {
+                helpSib.style.display = "none";
+            }
+        } else {
+            if (help != undefined) {
+                help.style.display = "";
+            }
+            if (helpSib != undefined) {
+                helpSib.style.display = "";
+            }
+        }
+        if (hmiRS == "Hide on All Platforms") {
+            if (rightSidebar != undefined) {
+                rightSidebar.style.display = "none";
+            }
+        } else {
+            if (rightSidebar != undefined) {
+                rightSidebar.style.display = "";
             }
         }
     }
 }
+
+// hopefully redundant code, kept only to make sure above is working properly on mobile, electron etc
+// I will remove in a future version if above working correctly
+
+/*
+    let electron = false;
+    if (window.roamAlphaAPI.platform.isMobileApp || window.roamAlphaAPI.platform.isDesktop) {
+        electron = true;
+    }
+
+    if (!electron) {
+        pf = document.querySelector("span.bp3-popover-wrapper:has(span.bp3-icon.bp3-icon-filter)");
+        pfSib = document.querySelector("span.bp3-popover-wrapper:has(span.bp3-icon.bp3-icon-filter) + .rm-topbar__spacer-sm");
+        calendar = document.querySelector("span.bp3-popover-wrapper:has(span.bp3-icon.bp3-icon-calendar)");
+        calendarSib = document.querySelector("span.bp3-popover-wrapper:has(span.bp3-icon.bp3-icon-calendar) + .rm-topbar__spacer-sm");
+        threeDot = document.querySelector("span.bp3-popover-wrapper:has(span.bp3-button.bp3-minimal.bp3-small.bp3-icon-more)");
+        threeDotSib = document.querySelector("span.bp3-popover-wrapper:has(span.bp3-button.bp3-minimal.bp3-small.bp3-icon-more) + .rm-topbar__spacer-sm");
+        width = document.querySelector("span.bp3-popover-wrapper:has(span.bp3-button.bp3-minimal.bp3-small.bp3-icon-horizontal-distribution)");
+        widthSib = document.querySelector("span.bp3-popover-wrapper:has(span.bp3-button.bp3-minimal.bp3-small.bp3-icon-horizontal-distribution) + .rm-topbar__spacer-sm");
+        help = document.querySelector("span.bp3-popover-wrapper:has(span.bp3-button.bp3-minimal.bp3-icon-help.bp3-small)");
+        helpSib = document.querySelector("span.bp3-popover-wrapper:has(span.bp3-button.bp3-minimal.bp3-icon-help.bp3-small) + .rm-topbar__spacer-sm");
+        rightSidebar = document.querySelector("span.bp3-popover-wrapper:has(span.bp3-icon-menu-closed)");
+    } else {
+        let topbar = document.querySelectorAll("div.rm-topbar > span.bp3-popover-wrapper");
+        if (topbar.length > 0) {
+            for (var i = 0; i < topbar.length; i++) {
+                if (topbar[i].innerHTML.indexOf("bp3-icon-filter") !== -1) {
+                    pf = topbar[i];
+                    pfSib = pf.nextSibling;
+                } else if (topbar[i].innerHTML.indexOf("bp3-icon-calendar") !== -1) {
+                    calendar = topbar[i];
+                    calendarSib = calendar.nextSibling;
+                } else if (topbar[i].innerHTML.indexOf("bp3-icon-more") !== -1) {
+                    threeDot = topbar[i];
+                    threeDotSib = threeDot.nextSibling;
+                } else if (topbar[i].innerHTML.indexOf("bp3-icon-horizontal-distribution") !== -1) {
+                    width = topbar[i];
+                    widthSib = width.nextSibling;
+                } else if (topbar[i].innerHTML.indexOf("bp3-icon-help") !== -1) {
+                    help = topbar[i];
+                    helpSib = help.nextSibling;
+                } else if (topbar[i].innerHTML.indexOf("bp3-icon-menu-closed") !== -1) {
+                    rightSidebar = topbar[i];
+                }
+            }
+        }
+    }
+*/
